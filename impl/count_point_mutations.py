5eef299df0ab4050e81e7a54714489d38df7f69f
nomaterials <nomaterials@gmail.com>
2018-06-21 23:55:04 +0300

    fix type error

--- impl/count_point_mutations.py
+++ impl/count_point_mutations.py
@@ -6,4 +6,4 @@ Hamming distance of two DNA strings on separate lines
 def count_point_mutations(data):
     dna_list = data.split()
 
-    return len(filter(lambda pair: pair[0] != pair[1], zip(list(dna_list[0]), list(dna_list[1]))))
+    return str(len(filter(lambda pair: pair[0] != pair[1], zip(list(dna_list[0]), list(dna_list[1])))))
